c8b10b9c777b2255416c4902059eba549e9d38a4
Ihar Abukhouski <ihar.abukhouski@gmail.com>
2021-11-15 18:24:20 +0100

    Configured React based client application

---
+++ client/src/presentation/index.ts
@@ -0,0 +1,13 @@
+/**
+ * This module is used to integrate 'Presentation' layers
+ * of domain modules (e.g. Products, Reviews) into a coherent whole
+ */
+
+/**************
+ * PUBLIC API *
+ **************/
+
+/**
+ * Components
+ */
+export { App } from './App';
